f0ba205ca88c99294224b16dd183cd197cf3fa08
Creeford Sanchez <creewrk@gmail.com>
2025-04-01 14:30:31 -0700

    Added Table Properties

--- Clinico.Models/Doctor.cs
+++ Clinico.Models/Doctor.cs
@@ -1,4 +1,14 @@
 ﻿namespace Clinico.Models {
     public class Doctor {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Specialty { get; set; }
+        public int RoomId { get; set; }
+        public int AppointmentId { get; set; }
+        public ICollection<ExamRoom> ExamRooms { get; set; }
+        public ICollection<Appointment> Appointments { get; set; }
     }
 }
--- Clinico.Models/Patient.cs
+++ Clinico.Models/Patient.cs
@@ -1,4 +1,10 @@
 ﻿namespace Clinico.Models {
     public class Patient {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public ICollection<Appointment> Appointments { get; set; }
     }
 }
