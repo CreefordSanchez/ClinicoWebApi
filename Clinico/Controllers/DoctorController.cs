e8fe65efc9b645453b8ea545e532be23f44e8105
Creeford Sanchez <creewrk@gmail.com>
2025-04-01 11:47:33 -0700

    added controllers

---
+++ Clinico/Controllers/DoctorController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Clinico.Controllers {
+    public class DoctorController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
---
+++ Clinico/Controllers/ExamRoomController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Clinico.Controllers {
+    public class ExamRoomController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
