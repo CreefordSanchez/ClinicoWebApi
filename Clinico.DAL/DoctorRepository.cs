dc085533671075b5b4c5ab01107ee9ee9e098350
Creeford Sanchez <creewrk@gmail.com>
2025-04-02 10:39:27 -0700

    add Doctor/Room CRUD's

--- Clinico.DAL/DoctorRepository.cs
+++ Clinico.DAL/DoctorRepository.cs
@@ -1,4 +1,38 @@
-﻿namespace Clinico.DAL {
+﻿using Clinico.Models;
+
+namespace Clinico.DAL {
     public class DoctorRepository {
+        private readonly ClinicoContext _context;
+        public DoctorRepository(ClinicoContext context) {
+            _context = context;
+        }
+
+        public void CreateDoctor(Doctor doctor) {
+            _context.Doctors.Add(doctor);
+            _context.SaveChanges();
+        }
+
+        public void RemoveDoctor(Doctor doctor) {
+            _context.Remove(doctor);
+            _context.SaveChanges();
+        }
+
+        public void UpdateDoctor(Doctor doctor) {
+            _context.Update(doctor);
+            _context.SaveChanges();
+        }
+
+        public void DeleteDoctor(Doctor doctor) {
+            _context.Remove(doctor);
+            _context.SaveChanges();
+        }
+
+        public Doctor GetDoctor(int id) {
+            return _context.Doctors.Find(id);
+        }
+
+        public List<Doctor> GetDoctorList() {
+            return _context.Doctors.ToList();
+        }
     }
 }
--- Clinico.DAL/ExamRoomRepository.cs
+++ Clinico.DAL/ExamRoomRepository.cs
@@ -1,4 +1,28 @@
-﻿namespace Clinico.DAL {
+﻿using Clinico.Models;
+
+namespace Clinico.DAL {
     public class ExamRoomRepository {
+        private readonly ClinicoContext _context;
+        public ExamRoomRepository(ClinicoContext context) {
+            _context = context;
+        }
+
+        public void RemoveExamRoom(ExamRoom Room) {
+            _context.ExamRooms.Remove(Room);
+            _context.SaveChanges();
+        }
+
+        public void UpdateExamRoom(ExamRoom Room) {
+            _context.ExamRooms.Update(Room);
+            _context.SaveChanges();
+        }
+
+        public ExamRoom GetExamRoom(int id) {
+            return _context.ExamRooms.Find(id);
+        }
+
+        public List<ExamRoom> GetExamRoomList() {
+            return _context.ExamRooms.ToList();
+        }
     }
 }
