d74f6139588fd16495dbe98a2e6db14aa8bb44c5
Creeford Sanchez <creewrk@gmail.com>
2025-04-02 10:57:31 -0700

    CRUD on Docter/Room services

--- Clinico.BLL/DoctorService.cs
+++ Clinico.BLL/DoctorService.cs
@@ -1,4 +1,54 @@
-﻿namespace Clinico.BLL {
+﻿using Clinico.DAL;
+using Clinico.Models;
+
+namespace Clinico.BLL {
     public class DoctorService {
+        //CRUD
+        private readonly DoctorRepository _repository;
+        
+        public DoctorService(DoctorRepository repository) {
+            _repository = repository;
+        }
+        public void CreateDoctor(Doctor doctor) {
+            if (doctor != null) {
+                _repository.CreateDoctor(doctor);
+            }
+        }
+        public void RemoveDoctor(Doctor doctor) {
+            if (doctor != null) {
+                _repository.RemoveDoctor(doctor);
+            }
+            
+        }
+
+        public void UpdateDoctor(Doctor doctor) {
+            if (doctor != null) {
+                _repository.UpdateDoctor(doctor);
+            }
+        }
+
+        public void DeleteDoctor(Doctor doctor) {
+            if (doctor != null) {
+                _repository.RemoveDoctor(doctor);
+            }
+        }
+
+        public Doctor GetDoctor(int id) {
+            if (id != null) {
+                return _repository.GetDoctor(id);
+            }
+
+            return new Doctor();
+        }
+
+        public List<Doctor> GetDoctorList() {
+            List<Doctor> list = _repository.GetDoctorList();
+
+            if (list != null) {
+                return list;
+            }
+
+            return new List<Doctor>();
+        }
     }
 }
--- Clinico.BLL/ExamRoomService.cs
+++ Clinico.BLL/ExamRoomService.cs
@@ -1,4 +1,41 @@
-﻿namespace Clinico.BLL {
+﻿using Clinico.DAL;
+using Clinico.Models;
+
+namespace Clinico.BLL {
     public class ExamRoomService {
+        private readonly ExamRoomRepository _repository;
+        public ExamRoomService(ExamRoomRepository repository) {
+            _repository = repository;
+        }
+
+        public void RemoveExamRoom(ExamRoom room) {
+            if (room != null) {
+                _repository.RemoveExamRoom(room);
+            }
+        }
+
+        public void UpdateExamRoom(ExamRoom room) {
+            if (room != null) {
+                _repository.UpdateExamRoom(room);
+            }
+        }
+
+        public ExamRoom GetExamRoom(int id) {
+            if (id != null) {
+                return _repository.GetExamRoom(id);
+            }
+
+            return new ExamRoom();
+        }
+
+        public List<ExamRoom> GetExamRoomsList() {
+            List<ExamRoom> list = _repository.GetExamRoomList();
+
+            if (list != null) {
+                return list; 
+            }
+
+            return new List<ExamRoom>;
+        }
     }
 }
